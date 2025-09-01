e1e9240e267cff866f3bcf72ec128d764770b92d
Ishu0712 <ishwarjeet.a7@gmail.com>
2025-09-02 00:24:24 +0530

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -2,6 +2,12 @@
     source= "../azurerm_resource_group"
     }
 
+# Create a resource group
+resource "azurerm_resource_group" "RG03" {
+  name     = "RG03"
+  location = "West Europe"
+}
+
 
   # module "child_storage"{
   #   source= "../azurerm_storage_account"
