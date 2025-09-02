18d3ef7fa54146da7aa2ec45604c92ea48b52739
Ishu0712 <ishwarjeet.a7@gmail.com>
2025-09-02 21:31:34 +0530

    Update provider.tf

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -7,8 +7,8 @@ terraform {
   }
   backend "azurerm" {
     resource_group_name  = "RG02"  
-    storage_account_name = "dev712"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "prestige"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    storage_account_name = "bajranbalistorage"                  # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "container"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "elm.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
     
