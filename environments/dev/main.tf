74877fc843e74610a0d776a366702bf2ebbda344
Ishu0712 <ishwarjeet.a7@gmail.com>
2025-09-02 10:39:35 +0530

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,8 +1,11 @@
-  module "mera_RG"{
-    source= "../../modules/azurerm_resource_group"
-    }
+ # module "mera_RG"{
+ #   source= "../../modules/azurerm_resource_group"
+ #   }
 
-
+resource "azurerm_resource_group" "RG03" {
+  name     = "RG03"
+  location = "West Europe"
+}
 
 
   # module "child_storage"{
