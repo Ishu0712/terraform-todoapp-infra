a152672dc9078ee917ab857aa6096afd393dc906
Ishu0712 <ishwarjeet.a7@gmail.com>
2025-09-02 13:07:32 +0530

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,11 +1,11 @@
 module "resource_group_1" {
   source   = "../../modules/azurerm_resource_group"
-  name     = "RG03"
+  name     = "RG05"
   location = "West Europe"
 }
 
 module "resource_group_2" {
   source   = "../../modules/azurerm_resource_group"
-  name     = "RG04"
+  name     = "RG06"
   location = "West Europe"
 }
