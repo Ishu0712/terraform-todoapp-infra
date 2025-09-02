fce322c84f850ddca69ba58e4aa5e9a53c640ab0
Ishu0712 <ishwarjeet.a7@gmail.com>
2025-09-02 12:30:02 +0530

    Update main.tf

--- modules/azurerm_resource_group/main.tf
+++ modules/azurerm_resource_group/main.tf
@@ -1,11 +1,5 @@
-# Create a resource group
-resource "azurerm_resource_group" "RG01" {
-  name     = "RG03"
-  location = "West Europe"
-}
-
-resource "azurerm_resource_group" "RG02" {
-  name     = "RG04"
-  location = "West Europe"
+resource "azurerm_resource_group" "this" {
+  name     = var.name
+  location = var.location
 }
 
